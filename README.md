0528fc0fdd055b452bc919742227fbe0cd8a4cdb
LaFeev <aaron.lafevers@gmail.com>
2022-09-29 14:57:55 -0500

    updated utils.get_dataset, removed partial data files, updated README.md

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# sf_crime
+# Analysis of crime statistics in San Francisco, CA <br>(2018 - Present)
+##### The University of Denver - MS Data Science <br>COMP 4447 - Data Science Tools I <br>Fall Quarter 2022
 
-### Analysis of crime statistics in San Fransisco, CA, with a focus on the time period before and after the COVID-19 pandemic (2018 -2022).
-
-### The Univeristy of Denver - MS Data Science - COMP 4447 - Data Science Tools I
-
+**Research question:**
+*Can we gain insights into the 'where', 'when', and 'why' of crime incidents in SF?*
+____
+Utilizing the datasf.org's Open Data collection, we start with a large dataset comprising police reports filed between 2018 and present*.  At the time of writing, this dataset is being actively updated and will continue to grow.  There is also an API provided by datasf.org to allow for updating the dataset without re-downloading.  This API can also be used in the future to rebuild the dataset should the original file cease to be hosted.
 [Date Source](https://data.sfgov.org/Public-Safety/Police-Department-Incident-Reports-2018-to-Present/wg3w-h783)
+The police reports have been anonymized by the data provider, and reports pertaining to juveniles have been redacted.  Reports filed with law enforcement agencies outside of SFPD are not included.  See more details about the original dataset [here](https://datasf.gitbook.io/datasf-dataset-explainers/sfpd-incident-report-2018-to-present).
+<small>\*This dataset was downloaded Sep/Oct 2022</small>
+____
 
--- utils/utils.py
+++ utils/utils.py
@@ -1,26 +1,30 @@
 from genericpath import isfile
 import pandas as pd
-import os.path
 
 
 def get_dataset(path_to_data):
-    '''reads/downloads the SF crime stats csv for 2018-2022 and saves it into 3 separate csv files'''
+    '''reads/downloads the SF crime stats csv for 2018-2022, trims columns, and returns a DataFrame'''
     fname = path_to_data + "Police_Department_Incident_Reports__2018_to_Present.csv"
     url = "https://data.sfgov.org/api/views/wg3w-h783/rows.csv?accessType=DOWNLOAD"
 
-    if not os.path.isfile(path_to_data + "full_data_1.csv") and \
-        not os.path.isfile(path_to_data + "full_data_2.csv") and \
-        not os.path.isfile(path_to_data + "full_data_3.csv"):
-        
-        try:
-            with open(fname, "r") as f:
-                full_data = pd.read_csv(f)
-        except FileNotFoundError:
-            # full dataset csv not found locally
-            full_data = pd.read_csv(url)
-        
-        # write the full dataset to the data directory in 3 chunks (to limit size < 100MB)
-        # note only full_data_1 will contain a header row
-        full_data.iloc[:213333].to_csv(path_to_data + "full_data_1.csv")
-        full_data.iloc[213333:426666].to_csv(path_to_data + "full_data_2.csv")
-        full_data.iloc[426666:].to_csv(path_to_data + "full_data_3.csv")
+    # this function gets called at the beginning of the notebook, every time.  If you want to change
+    #  which columns get dropped, update the remove_cols.dat file prior to running/rerunning the
+    #  notebook.  Because the columns are being read in from file, it isn't necessary to re-import
+    #  the utils module after making a change to the remove_cols.dat file.
+    cols_fname = path_to_data + "remove_cols.dat"
+
+    try:
+        # try to open the full data file
+        with open(fname, "r") as f:
+            full_data = pd.read_csv(f)
+    except FileNotFoundError:
+        # full dataset csv not found locally, go download it
+        full_data = pd.read_csv(url)
+        # write the full dataset to the data directory to avoid downloading next time
+        full_data.to_csv(fname, index=False)
+
+    # remove columns specified by cols_fname
+    cols = pd.read_csv(cols_fname)
+    trim_data = full_data.drop(cols['col'], axis = 1)
+
+    return trim_data
